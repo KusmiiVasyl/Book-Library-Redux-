8893c3ae83f96ea9efdef747d84c2aa73765429a
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-11-29 10:45:26 +0200

    Implement generate id for book using npm uuid

--- frontend/src/redux/books/reducer.js
+++ frontend/src/redux/books/reducer.js
@@ -1,10 +1,12 @@
 import * as actionTypes from './actionType'
+import { v4 as uuidv4 } from 'uuid';
 
 const initialState = []
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_BOOK:
+      action.payload.id = uuidv4()
       return [...state, action.payload]
     default:
       return state
