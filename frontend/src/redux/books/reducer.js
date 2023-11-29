24fc5b17d449915248b50fa0d8f1875c7daeb5a6
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-11-29 11:53:07 +0200

    Delete book by id in Redux Store

--- frontend/src/redux/books/reducer.js
+++ frontend/src/redux/books/reducer.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionType'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 const initialState = []
 
@@ -8,6 +8,8 @@ const booksReducer = (state = initialState, action) => {
     case actionTypes.ADD_BOOK:
       action.payload.id = uuidv4()
       return [...state, action.payload]
+    case actionTypes.DELETE_BOOK:
+      return state.filter((book) => book.id !== action.payload)
     default:
       return state
   }
