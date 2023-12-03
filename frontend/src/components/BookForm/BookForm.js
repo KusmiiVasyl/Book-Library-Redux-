459ae4b910da4faced18c10e1a77fd3cc0908e6c
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-12-03 15:41:57 +0200

    Added createAsyncThunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { addBook, thunkFunction } from '../../redux/slices/booksSlice'
+import { addBook, fetchBook } from '../../redux/slices/booksSlice'
 import bookData from '../../data/books.json'
 import createBookWithID from '../../utils/createBookWithID'
 import './BookForm.css'
@@ -25,7 +25,7 @@ const BookForm = () => {
   }
 
   const handlerAddRandomBookViaAPI = () => {
-    dispatch(thunkFunction)
+    dispatch(fetchBook())
   }
 
   return (
